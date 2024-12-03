04538ff1e3762137e86439c944081808fb9a62c0
joab <joabpereira356@gmail.com>
2024-12-03 00:08:01 -0300

    feat: Adicionando documentação swagger, beans validations e integração com api externa.

--- src/main/java/com/projeto_fuji/consultas/controller/ReceitaMedicaController.java
+++ src/main/java/com/projeto_fuji/consultas/controller/ReceitaMedicaController.java
@@ -3,6 +3,9 @@ package com.projeto_fuji.consultas.controller;
 import com.projeto_fuji.consultas.exception.ReceitaMedicaNotFoundException;
 import com.projeto_fuji.consultas.model.ReceitaMedica;
 import com.projeto_fuji.consultas.service.ReceitaMedicaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,15 +22,33 @@ public class ReceitaMedicaController {
     @GetMapping
     public List<ReceitaMedica> retornarTodasReceitas() {return receitaMedicaService.retornarTodasReceitas();}
 
+
+    @Operation(description = "Busca o receita médica por id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Retorna o receita médica por id"),
+            @ApiResponse(responseCode = "400", description = "Não existe a receita médica com o id informado")
+    })
     @GetMapping("/buscar/{id}")
     public Optional<ReceitaMedica> buscarReceita(@PathVariable Long id){return receitaMedicaService.buscarReceita(id);}
 
+
+    @Operation(description = "Cria uma nova receita médica")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Receita médica criada"),
+            @ApiResponse(responseCode = "400", description = "Erro ao criar receita médica")
+    })
     @PostMapping("/criar")
     public ResponseEntity<ReceitaMedica> criarReceita(@RequestBody ReceitaMedica receitaMedica){
         ReceitaMedica novaReceita = receitaMedicaService.criarReceita(receitaMedica);
         return ResponseEntity.status(HttpStatus.CREATED).body(novaReceita);
     }
 
+
+    @Operation(description = "Atualizando dados da receita médica")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Dados da receita médica atualizado"),
+            @ApiResponse(responseCode = "400", description = "Erro ao atualizar dados da receita médica")
+    })
     @PutMapping("/atualizar/{id}")
     public ResponseEntity<ReceitaMedica> atualizarReceita(@PathVariable Long id, @RequestBody ReceitaMedica receitaDetalhes){
         try{
@@ -40,6 +61,12 @@ public class ReceitaMedicaController {
         }
     }
 
+
+    @Operation(description = "Exclui receita médica por id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Receita médica exluida"),
+            @ApiResponse(responseCode = "400", description = "Erro ao excluir receita médica")
+    })
     @DeleteMapping("/deletar/{id}")
     public ResponseEntity<Void> deletarReceita(@PathVariable Long id){
         boolean deletado = receitaMedicaService.deletarReceita(id);
